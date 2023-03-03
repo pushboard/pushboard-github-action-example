4fb6d5569c57a2cd7ca0ee28faf5c9460e6347a2
WEI3M <126735714+WEI3M@users.noreply.github.com>
2023-03-03 10:45:57 -0800

    typo

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 # Trip Count
 ~/clickhouse local \
 -q "SELECT count(*) y1__num_trips
-FOM file('green_tripdata/*', 'Parquet') 
+FROM file('green_tripdata/*', 'Parquet') 
 FORMAT JSON 
 SETTINGS input_format_parquet_skip_columns_with_unsupported_types_in_schema_inference = True" |
 curl \

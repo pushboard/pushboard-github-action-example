26252cf142f93278874fbffeb68cf01ea52ffdba
WEI3M <126735714+WEI3M@users.noreply.github.com>
2023-03-06 11:26:03 -0800

    github action

--- script.sh
+++ script.sh
@@ -6,6 +6,7 @@
 FROM file('green_tripdata/*', 'Parquet') 
 FORMAT JSON 
 SETTINGS input_format_parquet_skip_columns_with_unsupported_types_in_schema_inference = True" |
+jq '. + {"title": "Total Trips"}' |
 curl \
   --request POST 'https://pushboard.io/api/carddata/4oi8rehmn3/' \
   --header 'Content-Type: application/json' \

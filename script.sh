9fdc8775f6f240a55c252fee1b64abb15a51d4f1
WEI3M <126735714+WEI3M@users.noreply.github.com>
2023-03-07 11:40:00 -0800

    remove action

--- script.sh
+++ script.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # Trip Count
-echo $1 |
+clickhouse-local \
+-q "SELECT count(*) y1__num_trips
+FROM file('green_tripdata/*', 'Parquet') 
+FORMAT JSON 
+SETTINGS input_format_parquet_skip_columns_with_unsupported_types_in_schema_inference = True" |
 jq '. + {"title": "Total Trips"}' |
 curl \
   --request POST 'https://pushboard.io/api/carddata/4oi8rehmn3/' \

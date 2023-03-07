cfdf45efaeaa5233d82e24c401cb61228005c944
WEI3M <126735714+WEI3M@users.noreply.github.com>
2023-03-07 10:20:16 -0800

    adding clickhouse local github action

--- script.sh
+++ script.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
 # Trip Count
-clickhouse-local \
--q "SELECT count(*) y1__num_trips
-FROM file('green_tripdata/*', 'Parquet') 
-FORMAT JSON 
-SETTINGS input_format_parquet_skip_columns_with_unsupported_types_in_schema_inference = True" |
+echo $1 |
 jq '. + {"title": "Total Trips"}' |
 curl \
   --request POST 'https://pushboard.io/api/carddata/4oi8rehmn3/' \

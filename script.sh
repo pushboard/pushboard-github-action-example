bc28cd43c4e9456e730d8eb966cec04416851f28
WEI3M <126735714+WEI3M@users.noreply.github.com>
2023-03-07 12:06:47 -0800

    update file location

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 # Trip Count
 clickhouse-local \
 -q "SELECT count(*) y1__num_trips
-FROM file('green_tripdata/*', 'Parquet') 
+FROM file('data/green_tripdata/*', 'Parquet') 
 FORMAT JSON 
 SETTINGS input_format_parquet_skip_columns_with_unsupported_types_in_schema_inference = True" |
 jq '. + {"title": "Total Trips"}' |
